925d293d152b370948f2cf1f39c5766c75554780
Panos30fyllou <panos30fyllou@gmail.com>
2020-05-13 05:23:39 +0300

    Create Coding Challenge #44.cs
    Coding Challenge 12/05/2020

---
+++ Coding Challenge #44.cs
@@ -0,0 +1,35 @@
+﻿using System;
+//@panos30fyllou
+namespace CC44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Type every number of the list seperated by ' ' and press Enter. Then, type one of them and press Enter.");
+                string[] input = Console.ReadLine().Split(" "); int x = int.Parse(Console.ReadLine());
+                int[] numList = new int[input.Length];
+                int index = -1; bool swaped = false;
+                for (int i = 0; i < numList.Length; i++)
+                {
+                    numList[i] = int.Parse(input[i]);
+                    if (numList[i] == x) index = i;
+                }
+                for (int i = 0; i < numList.Length; i++)
+                {
+                    if ((numList[i] > x && i < index) || (numList[i] < x && i >= index && numList[i] < numList[index + 1]))
+                    {
+                        numList[index] = numList[i];
+                        numList[i] = x;
+                        swaped = true;
+                    }
+                }
+                for (int i = 0; i < numList.Length; i++)
+                    if (i > 0 && numList[i] < numList[i - 1]) swaped = false;
+                Console.WriteLine(swaped ? "Swaped " + string.Join(" ", numList) : "Not Swaped");
+            }
+        }
+    }
+}
