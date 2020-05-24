87a514b390f8dc6f8b700ed73143f7102f305fff
Panos Triantafyllou <panos30fyllou@gmail.com>
2020-05-24 03:08:27 +0300

    24/05 Changes

---
+++ Next Gen Coder Challenges/Coding Challenge #39/Coding Challenge #39.py
@@ -0,0 +1,14 @@
+#@panos30fyllou
+def palindrome(s, l):
+    for i in range (l):
+        if (not s[i] == s[l - i - 1]):                   
+            return False   
+    return True
+
+s = list(input("Enter a string: "))
+size = len(s)
+i = 0
+while not palindrome(s, len(s)):
+        s.insert(size, s[i])
+        i += 1
+print(i)
---
+++ Next Gen Coder Challenges/Coding Challenge #45/Program.cs
@@ -0,0 +1,47 @@
+﻿//@panos30fyllou
+using System;
+
+namespace CC45
+{
+    class Trainer
+    {
+        public Trainer(string trainer_type, int trainer_attack, int trainer_health) {
+            Type = trainer_type;
+            Attack = trainer_attack;
+            Health = trainer_health;
+            Strong = false;
+        }
+
+        public bool Strong { get; set; }
+        public int Attack { get; set; }
+        public int Health { get; set; }
+        public string Type { get; set; }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Trainer tr1 = new Trainer(Console.ReadLine(), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            Trainer tr2 = new Trainer(Console.ReadLine(), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            if (tr1.Type.Equals("Fire")) {
+                if (tr2.Type.Equals("Grass"))       tr1.Strong = true;
+                else if (tr2.Type.Equals("Water"))  tr2.Strong = true;             
+            } else if (tr1.Type.Equals("Water")) {
+                if (tr2.Type.Equals("Fire"))        tr1.Strong = true;
+                else if (tr2.Type.Equals("Grass"))  tr2.Strong = true;
+            } else if (tr1.Type.Equals("Grass")) {
+                if (tr2.Type.Equals("Water"))       tr1.Strong = true;
+                else if (tr2.Type.Equals("Fire"))   tr2.Strong = true;
+            }
+            if (tr1.Strong) {   tr1.Attack *= 2;    tr2.Attack /= 2;    } 
+            else            {   tr1.Attack /= 2;    tr2.Attack *= 2;    }
+            while (true) {
+                tr2.Health -= tr1.Attack;
+                if (tr2.Health <= 0) break;
+                else tr1.Health -= tr2.Attack;
+                if (tr1.Health <= 0) break;
+            }
+            Console.WriteLine(tr2.Health <= 0? "Trainer 1 wins" : "Trainer 2 wins");
+        }
+    }
+}
